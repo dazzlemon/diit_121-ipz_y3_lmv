e5dd3798f05b93a434b2457fdae2206947c52b7c
dazzlemon <daanill13470@gmail.com>
2021-11-30 17:20:30 +0200

    closer

--- src/App.js
+++ src/App.js
@@ -4,62 +4,63 @@ import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
 const ReactApp = () => {
   const [counter, setCounter] = useState(0);
   return (
-    <View
-      style = {styles.view}
-    >
-      <Text
-        style = {styles.textMain}
-      >
-        Create an account
-      </Text>
+    <View style = {styles.screen}>
+      <View style = {styles.box}>
+        <Text
+          style = {styles.textMain}
+        >
+          Create an account
+        </Text>
 
-      <Text
-        style = {styles.textInputTitle}
-      >
-        email
-      </Text>
-      <TextInput
-        style = {styles.input}
-      />
+        <Text
+          style = {styles.textInputTitle}
+        >
+          email
+        </Text>
+        <TextInput
+          style = {styles.input}
+        />
 
-      <Text
-        style = {styles.textInputTitle}
-      >
-        username
-      </Text>
-      <TextInput
-        style = {styles.input}
-      />
+        <Text
+          style = {styles.textInputTitle}
+        >
+          username
+        </Text>
+        <TextInput
+          style = {styles.input}
+        />
 
-      <Text
-        style = {styles.textInputTitle}
-      >
-        password
-      </Text>
-      <TextInput
-        style = {styles.input}
-      />
+        <Text
+          style = {styles.textInputTitle}
+        >
+          password
+        </Text>
+        <TextInput
+          style = {styles.input}
+        />
 
-      <Text
-        style = {styles.textInputTitle}
-      >
-        date of birth
-      </Text>
-      <TextInput
-        style = {styles.input}
-      />
+        <Text
+          style = {styles.textInputTitle}
+        >
+          date of birth
+        </Text>
+        <TextInput
+          style = {styles.input}
+        />
 
-      <Button
-        title='Continue'
-      />
-      <Text>
-        Already have an account?
-      </Text>
+        <Button
+          title = 'Continue'
+          color = '#6665d2'
+        />
+        <Text>
+          Already have an account?
+        </Text>
 
-      <Text>
-        By registering, you agree to AppName Terms of Service and Privacy Policy.
-      </Text>
+        <Text>
+          By registering, you agree to AppName Terms of Service and Privacy Policy.
+        </Text>
 
+      </View>
     </View>
   )
 };
@@ -73,9 +74,10 @@ const styles = StyleSheet.create({
     margin: 2,
     backgroundColor: '#1e1e1e',
   },
-  view: {
-    padding: 100,
+  box: {
+    padding: 40,
     backgroundColor: '#1e2124',
+    borderRadius: 7,
   },
   textMain: {
     color: '#ffffff',
@@ -87,6 +89,12 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: '#707070',
   },
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 150,
+  }
 });
 
 export default ReactApp;
