415bbf51ad80bd364dc8b087c0ef52419a0044ee
dazzlemon <daanill13470@gmail.com>
2021-12-04 14:44:21 +0200

    register page visually done

--- src/webpages/Login.js
+++ src/webpages/Login.js
@@ -1,9 +1,61 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
+import { BoxedBg, CheckInput } from '../components';
+import { View } from 'react-native';
+import { styles } from '../theme';
+import { Link } from 'react-router-native';
 
-const Login = () => {
+const Login = () => (
+    <BoxedBg content = {<LoginForm/>}/>
+);
+
+const LoginForm = () => {
     return (
-        <Text>Login</Text>
+        <>
+            <View style = {styles.inBox}>
+                <Text
+                    style = {styles.textMain}
+                >
+                    Welcome back!
+                </Text>
+            </View>
+
+            <CheckInput
+                title = 'email'
+                isRequiredError = {null}
+                errorMessage = {() => null}
+            />
+
+            <CheckInput
+                title = 'password'
+                isRequiredError = {null}
+                secureTextEntry = {true}
+            />
+
+            <View style = {[styles.inBox, {marginTop: -15}]}>
+                <Text style = {false ? [styles.clickableText, styles.clickableTextHover]
+                                     : styles.clickableText}>
+                    Forgot your password?
+                </Text>   
+            </View>
+
+            <View style = {styles.inBox}>
+                <TouchableOpacity
+                    style = {styles.button}
+                >
+                    <Text style = {styles.buttonText}>
+                        Login
+                    </Text>
+                </TouchableOpacity>
+                
+                <Text style = {styles.tosText}>
+                    {'Need an account? '}
+                    <Text style = {styles.clickableText}>
+                        Register
+                    </Text>
+                </Text>
+            </View>
+        </>
     );
 };
 export default Login;
