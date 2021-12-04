0dcc1216c2f1ec7b98af45e3fdc1437b25cdd3eb
dazzlemon <daanill13470@gmail.com>
2021-12-04 14:21:58 +0200

    refactor

--- src/webpages/Register.js
+++ src/webpages/Register.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { View, ScrollView, Text, ImageBackground, TouchableOpacity } from 'react-native';
+import { View, ScrollView, Text, ImageBackground, TouchableOpacity, Image } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { Link } from 'react-router-native';
-import CheckInput from '../components/CheckInput';
+import { CheckInput } from '../components';
 import { styles } from '../theme';
 
 const bgImage = process.env.PUBLIC_URL + '/assets/images/bg.jpg';
@@ -11,6 +11,17 @@ function isEmail(str) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(str.toLowerCase());
 }
+
+const BoxedBg = (props) => {
+  return (<ImageBackground
+    source = {bgImage}
+    style = {styles.bg}
+  >
+    <ScrollView style = {styles.box}>
+      {props.content}
+    </ScrollView>
+  </ImageBackground>);
+};
 
 const Register = () => {
   const [hasAccount, setHasAccount] = useState(false);
@@ -32,160 +43,149 @@ const Register = () => {
   const [noPassword, setNoPassword] = useState(false);
 
   return (
-    <ImageBackground
-      source = {bgImage}
-      style = {styles.bg}
-    >
-      <ScrollView style = {styles.box}>
-        <View style = {styles.inBox}>
-          <Text
-            style = {styles.textMain}
-          >
-            Create an account
-          </Text>
-        </View>
+    <BoxedBg content = {(<>
+      <View style = {styles.inBox}>
+        <Text
+          style = {styles.textMain}
+        >
+          Create an account
+        </Text>
+      </View>
 
-        <CheckInput
-          title = {'email'}
-          isRequiredError = {noEmail}
-          errorMessage = {() => {
-            if (!isEmailValid) {
-              return 'please enter an email address';
-            }
-            if (isEmailRegistered) {
-              return 'email address already registered';
+      <CheckInput
+        title = {'email'}
+        isRequiredError = {noEmail}
+        errorMessage = {() => {
+          return !isEmailValid     ? 'please enter an email address'
+               : isEmailRegistered ? 'email address already registered'
+               : null;
+        }}
+        onChangeText = {(text) => setEmail(text)}
+      />
+
+      <CheckInput
+        title = {'username'}
+        isRequiredError = {noUsername}
+        onChangeText = {(text) => setUsername(text)}
+      />
+
+      <CheckInput
+        title = {'password'}
+        isRequiredError = {noPassword}
+        onChangeText = {(text) => setPassword(text)}
+        secureTextEntry = {true}
+      />
+
+      <View style = {styles.inBox}>
+        <Text style = {styles.textInputTitle}>
+          date of birth
+        </Text>
+
+        <View style = {{flexDirection: 'row', width: '100%'}}>
+          {/* Month */}
+          <Picker
+            style = {[styles.picker, {width: '40%'}]}
+            onValueChange = {(val, id) => setBirthMonth(val)}  
+          >
+            <Picker.Item label = 'Jan' value = '0' />
+            <Picker.Item label = 'Feb' value = '1' />
+            <Picker.Item label = 'Mar' value = '2' />
+          </Picker>
+
+          {/* Day */}
+          <Picker
+            style = {[styles.picker, {width: '20%', marginHorizontal: '10%'}]}
+            onValueChange = {(val, id) => setBirthday(val)}  
+          >
+            <Picker.Item label = '1' value = '1' />
+            <Picker.Item label = '2' value = '2' />
+            <Picker.Item label = '3' value = '3' />
+            <Picker.Item label = '28' value = '28' />
+            <Picker.Item label = '29' value = '29' />
+            <Picker.Item label = '30' value = '30' />
+            <Picker.Item label = '31' value = '31' />
+          </Picker>
+
+          {/* Year */}
+          <Picker
+            style = {[styles.picker, {width: '30%'}]}
+            onValueChange = {(val, id) => setBirthYear(val)}
+          >
+            <Picker.Item label = '2021' value = '2021' />
+            <Picker.Item label = '2020' value = '2020' />
+            <Picker.Item label = '2019' value = '2019' />
+          </Picker>
+        </View>
+        <Text style = {styles.textInputError}>
+          {!isValidBirthday ? 'Please enter a valid date of birth'
+                            : null}
+        </Text>
+      </View>
+
+      <View style = {styles.inBox}>
+        <TouchableOpacity
+          style = {styles.button}
+          onPress = {() => {
+            var noEmail_ = email == null || email == '';
+            setNoEmail(noEmail_);
+            var isEmailValid_ = !noEmail_ && isEmail(email);
+            setIsEmailValid(isEmailValid_);
+            var isEmailRegistered_ = isEmailValid_ && email == 'registered@email.com';
+            setIsEmailRegistered(isEmailRegistered_);
+
+            var birthdate = new Date(birthYear, birthMonth, birthday);
+            var isValidBirthday_ = birthdate.getDate() == birthday
+                                && birthdate.getFullYear() == birthYear
+                                && birthdate.getMonth() == birthMonth;
+            setIsValidBirthday(isValidBirthday_);
+
+            var noUsername = username == null || username == '';
+            setNoUsername(noUsername);
+            var noPassword = password == null || password == '';
+            setNoPassword(noPassword);
+
+            if (isEmailValid_ && !isEmailRegistered_ && isValidBirthday_ && !noPassword && !noUsername) {
+              register(email, username, password, birthdate);
             }
           }}
-          onChangeText = {(text) => setEmail(text)}
-        />
+        >
+          <Text style = {styles.buttonText}>
+            Continue
+          </Text>
+        </TouchableOpacity>
+        <Link 
+          onMouseEnter = {() => setHasAccount(true)}
+          onMouseLeave = {() => setHasAccount(false)}
+          to='/login'
+          underlayColor = 'rgba(0, 0, 0, 0)'
+        >
+          <Text style = {hasAccount ? [styles.clickableText, styles.clickableTextHover]
+                                    : styles.clickableText}>
+            Already have an account?
+          </Text>    
+        </Link>
 
-        <CheckInput
-          title = {'username'}
-          isRequiredError = {noUsername}
-          onChangeText = {(text) => setUsername(text)}
-        />
-
-        <CheckInput
-          title = {'password'}
-          isRequiredError = {noPassword}
-          onChangeText = {(text) => setPassword(text)}
-          secureTextEntry = {true}
-        />
-
-        <View style = {styles.inBox}>
+        <Text style = {styles.tosText}>
+          {'By registering, you agree to AppName '}
           <Text
-            style = {styles.textInputTitle}
-          >
-            date of birth
-            
-          </Text>
-          <View style = {{flexDirection: 'row', width: '100%'}}>
-
-            {/* Month */}
-            <Picker
-              style = {[styles.picker, {width: '40%'}]}
-              onValueChange = {(val, id) => setBirthMonth(val)}  
-            >
-              <Picker.Item label = 'Jan' value = '0' />
-              <Picker.Item label = 'Feb' value = '1' />
-              <Picker.Item label = 'Mar' value = '2' />
-            </Picker>
-
-            {/* Day */}
-            <Picker
-              style = {[styles.picker, {width: '20%', marginHorizontal: '10%'}]}
-              onValueChange = {(val, id) => setBirthday(val)}  
-            >
-              <Picker.Item label = '1' value = '1' />
-              <Picker.Item label = '2' value = '2' />
-              <Picker.Item label = '3' value = '3' />
-              <Picker.Item label = '28' value = '28' />
-              <Picker.Item label = '29' value = '29' />
-              <Picker.Item label = '30' value = '30' />
-              <Picker.Item label = '31' value = '31' />
-            </Picker>
-
-            {/* Year */}
-            <Picker
-              style = {[styles.picker, {width: '30%'}]}
-              onValueChange = {(val, id) => setBirthYear(val)}
-            >
-              <Picker.Item label = '2021' value = '2021' />
-              <Picker.Item label = '2020' value = '2020' />
-              <Picker.Item label = '2019' value = '2019' />
-            </Picker>
-          </View>
-          <Text style = {styles.textInputError}>
-              {!isValidBirthday ? 'Please enter a valid date of birth'
-                                : null}
-            </Text>
-        </View>
-
-        <View style = {styles.inBox}>
-          <TouchableOpacity
-            style = {styles.button}
-            onPress = {() => {
-              var noEmail_ = email == null || email == '';
-              setNoEmail(noEmail_);
-              var isEmailValid_ = !noEmail_ && isEmail(email);
-              setIsEmailValid(isEmailValid_);
-              var isEmailRegistered_ = isEmailValid_ && email == 'registered@email.com';
-              setIsEmailRegistered(isEmailRegistered_);
-
-              var birthdate = new Date(birthYear, birthMonth, birthday);
-              var isValidBirthday_ = birthdate.getDate() == birthday
-                                  && birthdate.getFullYear() == birthYear
-                                  && birthdate.getMonth() == birthMonth;
-              setIsValidBirthday(isValidBirthday_);
-
-              var noUsername = username == null || username == '';
-              setNoUsername(noUsername);
-              var noPassword = password == null || password == '';
-              setNoPassword(noPassword);
-
-              if (isEmailValid_ && !isEmailRegistered_ && isValidBirthday_ && !noPassword && !noUsername) {
-                register(email, username, password, birthdate);
-              }
-            }}
-          >
-            <Text style = {styles.buttonText}>
-              Continue
-            </Text>
-          </TouchableOpacity>
-          <Link 
-            onMouseEnter = {() => setHasAccount(true)}
-            onMouseLeave = {() => setHasAccount(false)}
-            to='/login'
-            underlayColor = 'rgba(0, 0, 0, 0)'
-          >
-            <Text style = {hasAccount ? [styles.clickableText, styles.clickableTextHover]
-                                      : styles.clickableText}>
-              Already have an account?
-            </Text>    
-          </Link>
-
-          <Text style = {styles.tosText}>
-            {'By registering, you agree to AppName '}
-            <Text
-              onMouseEnter = {() => setIsReadingTos(true)}
-              onMouseLeave = {() => setIsReadingTos(false)}
-              style = {isReadingTos ? [styles.clickableText, styles.clickableTextHover]
-                                    : styles.clickableText}
-              onPress = {() => alert('Redirect to ToS')}
-            >Terms of Service</Text>
-            {' and '} 
-            <Text
-              onMouseEnter = {() => setIsReadingPp(true)}
-              onMouseLeave = {() => setIsReadingPp(false)}
-              style = {isReadingPp ? [styles.clickableText, styles.clickableTextHover]
-                                   : styles.clickableText}
-              onPress = {() => alert('Redirect to PP')}
-            >Privacy Policy</Text>
-            .
-          </Text>
-        </View>
-      </ScrollView>
-    </ImageBackground>
+            onMouseEnter = {() => setIsReadingTos(true)}
+            onMouseLeave = {() => setIsReadingTos(false)}
+            style = {isReadingTos ? [styles.clickableText, styles.clickableTextHover]
+                                  : styles.clickableText}
+            onPress = {() => alert('Redirect to ToS')}
+          >Terms of Service</Text>
+          {' and '} 
+          <Text
+            onMouseEnter = {() => setIsReadingPp(true)}
+            onMouseLeave = {() => setIsReadingPp(false)}
+            style = {isReadingPp ? [styles.clickableText, styles.clickableTextHover]
+                                  : styles.clickableText}
+            onPress = {() => alert('Redirect to PP')}
+          >Privacy Policy</Text>
+          .
+        </Text>
+      </View>
+    </>)}/>
   )
 };
 
