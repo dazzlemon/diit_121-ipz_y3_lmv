2ae04faf9d71a4ff4c74faadbde9a1fc1c54bfc0
dazzlemon <daanill13470@gmail.com>
2021-12-05 16:19:33 +0200

    login page seems done

--- src/webpages/Login.js
+++ src/webpages/Login.js
@@ -11,6 +11,13 @@ const Login = () => (
 
 const LoginForm = () => {
     const [needsAccount, setNeedsAccount] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [noEmail, setNoEmail] = useState(false);
+    const [noPassword, setNoPassword] = useState(false);
+    const [forgotPassword, setForgotPassword] = useState(false);
+    const [badCredentials, setBadCredentials] = useState(false);
+    const [emailDoesntExist, setEmailDoesntExist] = useState(false);
 
     return (
         <>
@@ -24,19 +31,47 @@ const LoginForm = () => {
 
             <CheckInput
                 title = 'email'
-                isRequiredError = {null}
-                errorMessage = {() => null}
+                isRequiredError = {noEmail}
+                onChangeText = {(text) => setEmail(text)}
+                errorMessage = {() => badCredentials   ? 'email or password is invalid' 
+                                    : emailDoesntExist ? 'email does not exist'
+                                    : null}
             />
 
             <CheckInput
                 title = 'password'
-                isRequiredError = {null}
+                isRequiredError = {noPassword}
                 secureTextEntry = {true}
+                onChangeText = {(text) => setPassword(text)}
+                errorMessage = {() => badCredentials ? 'email or password is invalid' : null}
             />
 
-            <View style = {[styles.inBox, {marginTop: -14}]}>
-                <Text style = {false ? [styles.clickableText, styles.clickableTextHover]
-                                     : styles.clickableText}>
+            <View
+                style = {[styles.inBox, {marginTop: -14}]}
+                onMouseEnter = {() => setForgotPassword(true)}
+                onMouseLeave = {() => setForgotPassword(false)}    
+            >
+                <Text
+                    style = {forgotPassword ? [styles.clickableText, styles.clickableTextHover]
+                                            : styles.clickableText}
+                    onPress = {() => {
+                        // reset
+                        setNoPassword(false);
+                        setBadCredentials(false);
+
+                        setNoEmail(!email);
+                        if (email == 'registered@email.com') {
+                            setEmailDoesntExist(false);
+                            alert(`
+We sent instructions to change your password to
+${email}, please check both your inbox
+and spam folder.
+                            `);
+                        } else {
+                            setEmailDoesntExist(true);
+                        }
+                    }}                            
+                >
                     Forgot your password?
                 </Text>   
             </View>
@@ -44,6 +79,21 @@ const LoginForm = () => {
             <View style = {styles.inBox}>
                 <TouchableOpacity
                     style = {styles.button}
+                    onPress = {() => {
+                        setNoPassword(!password);
+                        setNoEmail(!email);
+
+                        if (!email || !password) {
+                            return;
+                        }
+
+                        if (email == 'registered@email.com' && password == '12345') {
+                            setBadCredentials(false);
+                            alert('successfull login');
+                        } else {
+                            setBadCredentials(true);
+                        }
+                    }}
                 >
                     <Text style = {styles.buttonText}>
                         Login
